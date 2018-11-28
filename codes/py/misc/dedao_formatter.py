2281d459d2e114ee5780f807e8c023f3f040f18f
yanz <dirtysalt1987@gmail.com>
2018-11-28 17:12:59 +0800

    fix

--- codes/py/misc/dedao_formatter.py
+++ codes/py/misc/dedao_formatter.py
@@ -5,10 +5,9 @@
 import glob
 import hashlib
 import io
+import jinja2
 import json
 import os
-
-import jinja2
 import requests
 
 style_string = """
@@ -268,7 +267,7 @@ def parse_response(resp):
         elif c['type'] == 'split':
             fh.write('<hr/>')
         else:
-            #print(c)
+            # print(c)
             pass
         fh.write('\n')
     return title, fh.getvalue()
@@ -342,19 +341,52 @@ index_string = """
 </html>
 """
 
+page_string = """
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta  http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta  name="viewport" content="width=device-width, initial-scale=1" />
+<title>{{ title }}</title>
+<meta  name="generator" content="Org-mode" />
+<meta  name="author" content="dirtysalt" />
+{{ style_string }}
+</head>
+
+<body>
+
+<div class="outline-2">
+<h2>{{ title }}</h2>
+<div class="outline-text-2">
+{{ html }}
+</div>
+</div>
+
+</body>
+</html>
+"""
+
+
 def main():
     fs = glob.glob('resp/get*')
     items = []
     dup = set()
     os.makedirs('html', exist_ok=True)
     os.makedirs('images', exist_ok=True)
+    template = jinja2.Template(page_string)
     for resp in fs:
         title, html = parse_response(resp)
         if title in dup: continue
         dup.add(title)
         path = "html/{}.html".format(title)
         with open(path, 'w') as fh:
-            fh.write(html)
+            output = template.render(style_string=style_string,
+                                     title=title,
+                                     html=html)
+            output = output.replace('images/', '../images/')
+            fh.write(output)
         items.append((title, html, path))
     items.sort(key=lambda x: x[0])
 
