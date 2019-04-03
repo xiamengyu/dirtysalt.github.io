d50b57657ab9f1bb0c2fc8ab9f0c411ae89dadcc
yanz <dirtysalt1987@gmail.com>
2019-04-03 11:39:41 +0800

    fix

--- codes/py/nginx-upload/app.py
+++ codes/py/nginx-upload/app.py
@@ -1,41 +1,49 @@
 #!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
-
-import web
-
-urls = ('/upload', 'upload',
-        '/.*', 'home')
-
-app = web.application(urls, globals())
-
-class home:
-    def GET(self):
-        return """<html><head></head><body>
-<form method="POST" enctype="multipart/form-data" action="/upload">
-<input type="file" name="myfile" />
-<br/>
-<input type="submit" />
-</form>
-</body></html>"""
-
+# coding:utf-8
+# Copyright (C) dirlt
 import cgi
-class upload:
-    def POST(self):
-        s = ''
-        winput = web.input()
-        for k in list(winput.keys()):
-            s += '<li>%s = %s</li>' % (k, cgi.escape(str(winput[k])))
-        ctx_s = ''
-        for k in list(web.ctx.keys()):
-            ctx_s += '<li>%s = %s</li>' % (k, cgi.escape(str(web.ctx[k])))
-        env_s = ''
-        for k in list(web.ctx.environ.keys()):
-            env_s += '<li>%s = %s</li>' % (k, cgi.escape(str(web.ctx.environ[k])))
-        html = """<html><body><p>web input<br/><ul>%s</ul></p>
-        <p>web ctx<br/><ul>%s</ul></p>
-        <p>web ctx environ<br/><ul>%s</ul></p></body></html>""" % (s, ctx_s, env_s)
-        return html
+import flask
+from flask import request as AppRequest
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def handle_index():
+    return """<html><head></head><body>
+    <form method="POST" enctype="multipart/form-data" action="/upload">
+    <input type="file" name="myfile" />
+    <br/>
+    <input type="submit" />
+    </form>
+    </body></html>"""
+
+
+@app.route('/upload', methods=['POST'])
+def handle_upload():
+    s = ''
+    args = AppRequest.args
+    for k in list(args.keys()):
+        s += '<li>%s = %s</li>' % (k, cgi.escape(str(args[k])))
+    form_s = ''
+    form = AppRequest.form
+    for k in list(form.keys()):
+        form_s += '<li>%s = %s</li>' % (k, cgi.escape(str(form[k])))
+    headers_s = ''
+    headers = AppRequest.headers
+    for k in list(headers.keys()):
+        headers_s += '<li>%s = %s</li>' % (k, cgi.escape(str(headers[k])))
+    files_s = ''
+    files = AppRequest.files
+    for k in list(files.keys()):
+        f = files[k]
+        files_s += '<li>{} = ({}, {}, {})</li>'.format(f.name, f.filename, f.content_type, f.content_length)
+    html = """<html><body><p>args<br/><ul>%s</ul></p>
+    <p>form<br/><ul>%s</ul></p>
+    <p>headers<br/><ul>%s</ul></p>
+    <p>files<br/><ul>%s</ul></body></html>""" % (s, form_s, headers_s, files_s)
+    return html
+
 
 if __name__ == "__main__":
-   app.run()
+    app.run(port=9999, debug=True)
