951b512b1a11f34c3fd98b261b1725dd862cb082
yanz <dirtysalt1987@gmail.com>
2019-05-14 15:15:26 +0800

    strict fields for golang

--- codes/py/pb_json_msgpack_comp/test.go
+++ codes/py/pb_json_msgpack_comp/test.go
@@ -7,6 +7,11 @@ import (
 	// jsoniter "github.com/json-iterator/go"
 )
 
+type JSObj struct {
+	Keys []string `json:"keys"`
+	Pids []int    `json:"pids"`
+}
+
 func json_bench(n int) {
 	// var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
@@ -17,19 +22,22 @@ func json_bench(n int) {
 		keys[i] = fmt.Sprintf("key#%d", i)
 		pids[i] = i + 200000
 	}
-	obj := make(map[string]interface{})
-	obj["keys"] = keys
-	obj["pids"] = pids
+	// obj := make(map[string]interface{})
+	// obj["keys"] = keys
+	// obj["pids"] = pids
+	obj := JSObj{Keys: keys, Pids: pids}
+	// fmt.Println(obj)
 
 	var data []byte
 	start := time.Now()
 	for i := 0; i < n; i++ {
-		data, _ = json.Marshal(obj)
+		data, _ = json.Marshal(&obj)
 	}
 	end := time.Now()
 	fmt.Printf("json save = %.2f, size = %d\n", end.Sub(start).Seconds(), len(data))
 
-	obj2 := make(map[string]interface{})
+	// obj2 := make(map[string]interface{})
+	obj2 := JSObj{}
 	start = time.Now()
 	for i := 0; i < n; i++ {
 		json.Unmarshal(data, &obj2)
